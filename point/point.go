969572a78c5099d298bfc5799cb60ae8033263c7
Patrick Haun <patrick@bomgar.email>
2023-12-17 13:53:59 +0100

    manhattan distance

--- point/point.go
+++ point/point.go
@@ -1,5 +1,13 @@
-package model
+package point
+
+import "github.com/bomgar/adventhelper/math"
 
 type Point struct {
 	Y, X int
 }
+
+func ManhattanDistance(p1, p2 Point) int {
+    return math.Abs(p1.X-p2.X) + math.Abs(p1.Y-p2.Y)
+}
+
+
---
+++ point/point_test.go
@@ -0,0 +1,15 @@
+package point
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestManhattanDistance(t *testing.T) {
+
+    p1:=Point{1,2}
+    p2:=Point{10,3}
+	assert.Equal(t, 10, ManhattanDistance(p1, p2))
+}
+
